1da472ba3ead164ebeae13892762c6846b85a0fa
Murfidar <murfidar@gmail.com>
2023-12-28 15:36:50 +0700

    chore: refactor sdk composer.json and index.php

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 Dotenv\Dotenv::createUnsafeImmutable(__DIR__ . '/..' . '')->load();
 
-use BRI\InfoRekening\InfoRekening;
+use BRI\Balance\Balance;
 
 // env values
 $clientId = $_SERVER['BRI_CLIENT_KEY']; // customer key
@@ -20,13 +20,4 @@ $accessTokenPath = '/snap/v1.0/access-token/b2b'; //access token path
 $account = '111231271284142'; // account number
 $partnerId = ''; //partner id
 
-echo (new InfoRekening())->getInfoRekening(
-  $account,
-  $clientId,
-  $clientSecret,
-  $pKeyId,
-  $partnerId,
-  $baseUrl,
-  $path,
-  $accessTokenPath
-);
+echo (new Balance())->inquiry($account, $clientId, $clientSecret, $pKeyId, $partnerId, $baseUrl, $path, $accessTokenPath);
